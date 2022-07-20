896cbe63d48e142a21752dd7f46d043cd641d663
Victor Barajas <victorbarajas@Victors-MacBook-Pro.local>
2022-07-19 19:50:25 -0700

    coding on index.js to add the value according to the questions

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+console.log(weeklyExpensesQuestions)
+
+for(let i = 0; i < weeklyExpensesQuestions.length; i++){
+    let response = window.prompt(weeklyExpensesQuestions[i])
+    weeklyExpenses = weeklyExpenses+response
+
+    console.log(weeklyExpenses)
+}
